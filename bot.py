bbf5ca0ffeaf616b073acb37a860dd6122a9fb8a
rupammm69 <126093162+rupammm69@users.noreply.github.com>
2023-02-22 23:40:45 +0530

    test

---
+++ bot.py
@@ -0,0 +1,6 @@
+print("Hi there, Welcome to ABC Cafe customer support.How can I help you ?")
+i = input("Press : 1 for 'Menu'; 2 for location of our nearby store; 3 book our cafe for any celebrations or parties; 4 for other queries")
+if i == 1:
+    print("Press: 1 for Breads; 2 for Momos; 3 for Sandwichws; 0 for exit")
+if i == 2:
+    print("Location.....")    
