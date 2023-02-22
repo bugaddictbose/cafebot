41b9e75f7c14ed65a4fc4daf491e4231ad8670ca
rupammm69 <126093162+rupammm69@users.noreply.github.com>
2023-02-22 21:34:37 +0530

    Update botmain.py

--- botmain.py
+++ botmain.py
@@ -1,1 +1,2 @@
-print("hello world")
+print("Hi there, Welcome to ABC Cafe customer support.How can I help you ?")
+i = input("Press : 1 for 'Menu'; 2 for location of our nearby store; 3 book our cafe for any celebrations or parties; 4 for other queries")
